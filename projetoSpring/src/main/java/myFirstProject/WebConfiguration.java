1245fdf1e021d0737ffef9089ef80cb1dd09f950
Alan Paulo <alanpaullo@yahoo.com.br>
2022-02-01 20:49:25 -0300

    Create project

---
+++ projetoSpring/src/main/java/myFirstProject/WebConfiguration.java
@@ -0,0 +1,13 @@
+package myFirstProject;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean(name="applicationName")
+    public String applicationName(){
+        return "Sistema de Vendas";
+    }
+}
