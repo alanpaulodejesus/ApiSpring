f4ef940f99adcbdc3099760904ccf98cb338e29a
alan <you@example.com>
2022-02-04 09:47:24 -0300

    Update projects using BD

--- projetoSpring/src/main/java/myFirstProject/VendasApplication.java
+++ projetoSpring/src/main/java/myFirstProject/VendasApplication.java
@@ -9,20 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
 
-     @Value("${application.name}") //--> Valor direto da propriedade application.properties
-     /*
-     @Autowired
-     @Qualifier("applicationName")
-    */
-    private String applicationName;
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return applicationName;
-    }
     public static void main(String[] args) {
         SpringApplication.run( VendasApplication.class,args );
     }
