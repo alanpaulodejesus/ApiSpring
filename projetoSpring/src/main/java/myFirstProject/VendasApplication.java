582baee97d6c1ac80423bd42524fa9a7b9177b6f
alan <you@example.com>
2022-02-07 22:45:55 -0300

    Create BD using jdbc

--- projetoSpring/src/main/java/myFirstProject/VendasApplication.java
+++ projetoSpring/src/main/java/myFirstProject/VendasApplication.java
@@ -1,15 +1,54 @@
 package myFirstProject;
 
+import myFirstProject.domain.enty.Cliente;
+import myFirstProject.domain.repositorio.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+
+            clientes.salvar(new Cliente("Alan"));
+            clientes.salvar(new Cliente("Alan II"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " Atualizado!");
+                clientes.atualizar(c);
+            });
+
+            System.out.println("Buscando..");
+            clientes.buscarPorNome("Alan").forEach(System.out::println);
+
+            System.out.println("Deletando..");
+            clientes.obterTodos().forEach(c->{
+                clientes.deletar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum encontrado");
+            }else {
+                todosClientes.forEach(System.out::println);
+            }
+            /*
+            Cliente cliente2 = new Cliente();
+            cliente2.setNome("Alan II");
+            clientes.salvar(cliente);
+            */
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run( VendasApplication.class,args );
---
+++ projetoSpring/src/main/java/myFirstProject/domain/enty/Produto.java
@@ -0,0 +1,36 @@
+package myFirstProject.domain.enty;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    private String descricao;
+    private BigDecimal preco;
+
+}
