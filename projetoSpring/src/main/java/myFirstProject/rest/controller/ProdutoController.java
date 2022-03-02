5bc706b16ca406fbb78e89a49d4b9c5f56d507bc
alan <you@example.com>
2022-03-01 23:38:27 -0300

    update spring web

---
+++ projetoSpring/src/main/java/myFirstProject/rest/controller/ProdutoController.java
@@ -0,0 +1,71 @@
+package myFirstProject.rest.controller;
+
+import myFirstProject.domain.entity.Cliente;
+import myFirstProject.domain.entity.Produto;
+import myFirstProject.domain.repository.Clientes;
+import myFirstProject.domain.repository.Produtos;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/produtos")
+public class ProdutoController {
+
+    private Produtos produtos;
+
+    public ProdutoController(Produtos produtos) {
+        this.produtos = produtos;
+    }
+
+    @GetMapping("{id}")
+    public Produto getProdutoById(@PathVariable Integer id){
+        return produtos.findById(id).orElseThrow(()->new ResponseStatusException( HttpStatus.NOT_FOUND, "Produto não encontrado"));
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto save(@RequestBody Produto produto){
+        return produtos.save(produto);
+
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        produtos.findById(id).map(produto -> {produtos.delete(produto);
+            return produto;
+        }).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+    }
+
+    //@RequestMapping(method = RequestMethod.PUT)
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id,
+                       @RequestBody Produto produto){
+
+        produtos
+                .findById(id)
+                .map(produtoExistente-> {
+                    produto.setId(produtoExistente.getId()); //--> Apenas o map pode buscar todos as informações do produto pelo id
+                    produtos.save(produto);
+                    return produto;
+                }).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+
+    }
+
+    @GetMapping
+    public List<Produto> find(Produto filtro){
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+
+        return produtos.findAll(example);
+
+    }
+
+}
